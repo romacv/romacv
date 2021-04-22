819ddae6ef4f2f803fed5a67083c454de8832f18
Roman Resenchuk <r@resrom.com>
2021-04-22 23:25:09 +0300

    initial

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php include 'header.inc' ?>
+  <main>
+    <div class="container">
+      <a name="aboutme"></a>
+      <section class="aboutme">
+        <h2>About me</h2>
+        <div class="row">
+          <div class="col-xl-4 col-md-5 photo">
+            <img src="../img/avatar.jpg" class="img-fluid" alt="Roman Resenchuk">
+          </div>
+          <div class="col-xl-8 col-md-7">
+            <p>Software development engineer, programmer, manager. <br>Currently I’m engaged in developing software for mobile (iOS app).  I’m able to coordinate work of team to develop high-quality projects, consult the client and colleagues or take the project into independent work.</p>
+            <p>My hobby is information technologies, computers, cars, travel, new cognition.</p>
+            <p>Working with me, you get:</p>
+            <ul>
+              <li>— a responsible attitude to the task at hand;</li>
+              <li>— understanding and courtesy;</li>
+              <li>— efficiency in work and sticking to agreements.</li>
+            </ul>
+            <!--<div class="links">-->
+            <!--  <p>Internet:</p>-->
+            <!--  <a target="_blank" href="https://linkedin.com/in/romanres" rel="noopener">LinkedIn.com (prof)</a>-->
+            <!--  <a target="_blank" href="https://www.facebook.com/romanresenchuk" rel="noopener">Facebook</a>-->
+            <!--</div>-->
+          </div>
+        </div>
+      </section>
+      <hr>
+      <section class="topprojects">
+        <h2>Top projects</h2>
+        <div class="row">
+          <div class="col-sm-6">
+            <a href="../projects" style="background-image:url('../img/works/readyforsky.jpg')" data-name="READY FOR SKY"></a>
+          </div>
+          <div class="col-sm-6">
+            <a href="../projects" style="background-image:url('../img/works/autorent.jpg')" data-name="AUTO.RENT"></a>
+          </div>
+        </div>
+        <a href="../projects" class="viewall">All projects</a>
+      </section>
+      <a name="contactme"></a>
+      <hr>
+      <section class="contactme">
+        <h2>Contact me</h2>
+        <div class="row justify-content-center">
+          <div class="col-xl-5 col-lg-6 col-md-7 col-sm-8">
+            <p>I am always opened for a communication and constructive dialogue.</p>
+            <p><a href="mailto:R@RESROM.COM" target="_blank" rel="noopener">
+              <img src="../img/envelope.svg" alt="R@RESROM.COM">
+              R@RESROM.COM
+            </a></p>
+            <p><a href="https://facebook.com/romanresenchuk" target="_blank" rel="noopener">
+              <img src="../img/if_facebook_386622.svg" alt="facebook romanresenchuk">
+              Facebook
+            </a></p>
+            <p><a href="https://linkedin.com/in/romanres" target="_blank" rel="noopener">
+              <img src="../img/if_linkedin_386655.svg" alt="linkedin romanres">
+              LinkedIn
+            </a></p>
+            <p><a href="../RESUME_RESENCHUK.pdf" target="_blank" rel="noopener">
+              <img src="../img/resume.svg" alt="linkedin romanres">
+              Resume
+            </a></p>
+          </div>
+        </div>
+      </section>
+      <hr>
+    </div>
+  </main>
+<?php include 'footer.inc' ?>
